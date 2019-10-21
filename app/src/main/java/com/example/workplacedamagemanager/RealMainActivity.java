16384034eee8fa4439ca02ef0fd06b754f0ac6c8
rguptavh <rguptavh119@gmail.com>
2019-10-20 23:44:21 -0500

    New menu screen

--- app/src/main/java/com/example/workplacedamagemanager/RealMainActivity.java
+++ app/src/main/java/com/example/workplacedamagemanager/RealMainActivity.java
@@ -1,85 +1,26 @@
 package com.example.workplacedamagemanager;
 
 import android.content.Intent;
-import android.database.Cursor;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.Switch;
-import android.widget.Toast;
-import android.app.AlertDialog;
+import android.view.View;
+
 import android.widget.CompoundButton;
 
-import android.content.Context;
-import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
 
 public class RealMainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
 
     public static Boolean toggle = false;
     Switch switch1;
-    /*private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
-    private ShakeDetector mShakeDetector;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // ShakeDetector initialization
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager
-                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mShakeDetector = new ShakeDetector();
-        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
-
-            @Override
-            public void onShake(int count) {
-                *//*
-                 * The following method, "handleShakeEvent(count):" is a stub //
-                 * method you would use to setup whatever you want done once the
-                 * device has been shook.
-                 *//*
-                Toast.makeText(RealMainActivity.this, "Shook!!!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        // Add the following line to register the Session Manager Listener onResume
-        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
-    }
-
-    @Override
-    public void onPause() {
-        // Add the following line to unregister the Sensor Manager onPause
-        mSensorManager.unregisterListener(mShakeDetector);
-        super.onPause();
-    }*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         setContentView(R.layout.main);
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-         switch1 = (Switch)findViewById(R.id.detect);
-         switch1.setChecked(toggle);
+        switch1 = (Switch)findViewById(R.id.detect);
+        switch1.setChecked(toggle);
         switch1.setOnCheckedChangeListener(this);
 
         return true;
@@ -90,11 +31,11 @@ public class RealMainActivity extends AppCompatActivity implements CompoundButto
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch (item.getItemId()) {
-            case R.id.filereport:
+            case R.id.item2:
                 Intent intent1 = new Intent(this, Record.class);
                 this.startActivity(intent1);
                 return true;
-            case R.id.database:
+            case R.id.item4:
                 Intent intent2 = new Intent(this, MainActivity.class);
                 this.startActivity(intent2);
                 return true;
@@ -116,5 +57,12 @@ public class RealMainActivity extends AppCompatActivity implements CompoundButto
             stopService(new Intent(this, ShakeService.class));
         }
     }
+    public void filereport(View view) {
+        Intent intent1 = new Intent(this, Record.class);
+        this.startActivity(intent1);
+    }
+    public void database(View view) {
+        Intent intent1 = new Intent(this, MainActivity.class);
+        this.startActivity(intent1);
+    }
 }
-
